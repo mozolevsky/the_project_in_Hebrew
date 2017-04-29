56db9ca9da9edf292dae3f5cffd3d26267e798c1
mozolevsky <alf.rulit@gmail.com>
2017-04-29 20:11:32 +0300

    Accordion block finished, made reviews block for desctop version

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -20,3 +20,25 @@ $( window ).resize(function() {
     }
 
 });
+
+// accordion
+
+ function toggleIcon(e) {
+     $(e.target)
+         .prev('.panel-heading')
+         .find(".accordion__marker")
+         .toggleClass('accordion__close accordion__open');
+ }
+ $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+ $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+ // slider
+
+ var reviewsSlider = new Swiper('.reviews__slider', {
+     pagination: '.swiper-pagination',
+     paginationClickable: true,
+     nextButton: '.reviews__right-arrow',
+     prevButton: '.reviews__left-arrow',
+     spaceBetween: 30
+ });
